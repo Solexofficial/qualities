fededed24b8f4757fe8cfe7c89ba1682c86fd4d1
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-14 15:06:26 +0300

    feat: add redirect to "/" after create||update qualities

--- src/app/pages/addQuality.jsx
+++ src/app/pages/addQuality.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import QualityForm from '../components/ui/qualityForm';
 import { useQualities } from '../hooks/useQualities';
+import { useHistory } from 'react-router';
+
 const AddQualityPage = () => {
+  const history = useHistory();
   const { addQuality } = useQualities();
   const handleSubmit = data => {
-    addQuality(data);
-    console.log(data);
+    addQuality(data).then(data => {
+      if (data) history.push('/');
+    });
   };
 
   return (
--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import QualityForm from '../components/ui/qualityForm';
 import { useQualities } from '../hooks/useQualities';
 
 const EditQualityPage = () => {
   const id = useParams().id;
+  const history = useHistory();
   const { getQuality, updateQuality } = useQualities();
   const quality = getQuality(id);
 
   const handleSubmit = data => {
-    updateQuality(data);
+    updateQuality(data).then(data => {
+      if (data) history.push('/');
+    });
   };
 
   return (
